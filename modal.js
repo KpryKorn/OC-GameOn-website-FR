8d9d0d6457427a05232d4fa1858396aeea5479ee
KpryKorn <roffinisacha@gmail.com>
2024-01-26 17:20:05 +0100

    feat: add form validaton and error msg

--- modal.js
+++ modal.js
@@ -8,22 +8,42 @@ function editNav() {
   }
 }
 
-// DOM Elements
+/** ------------------------ DOM ELEMENTS ------------------------ **/
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 
 const formContainer = document.getElementById("formContainer");
 const formData = document.querySelectorAll(".formData");
 const formConfirmationModal = document.getElementById("formConfirmationModal");
+const formErrorMsg = document.getElementById("formNotValid");
 
 const closeModalBtn = document.getElementById("closeModalBtn");
 const closeConfirmationModalBtn = document.getElementById(
   "closeConfirmationModalBtn"
 );
 const submitBtn = document.getElementById("submitBtn");
-// TODO: add form elements
 
-// launch modal event
+/** ------------------------ FORM VALIDATION ------------------------ **/
+let formIsValid = false;
+submitBtn.disabled = true;
+
+if (formIsValid) {
+  submitBtn.disabled = false;
+
+  // ajouter confirmation quand le submit est réussi
+  formContainer.addEventListener("submit", (e) => {
+    e.preventDefault();
+    modalbg.style.display = "none";
+    formConfirmationModal.style.display = "block";
+  });
+} else {
+  formErrorMsg.textContent = "Le formulaire d'inscription n'est pas valide";
+}
+// TODO: add error msg if form is not valid
+// TODO: add form validation for each input
+// TODO: save form data if validation fails
+
+/** ------------------------ HANDLING MODALS ------------------------ **/
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
@@ -34,21 +54,8 @@ function launchModal() {
 // ferme la modale
 closeModalBtn.addEventListener("click", () => (modalbg.style.display = "none"));
 
-// ajouter confirmation quand le submit est réussi
-formContainer.addEventListener("submit", (e) => {
-  e.preventDefault();
-  modalbg.style.display = "none";
-  formConfirmationModal.style.display = "block";
-});
-
 // fermer modale de confirmation
 closeConfirmationModalBtn.addEventListener(
   "click",
   () => (formConfirmationModal.style.display = "none")
 );
-
-// TODO: disable submit button if form is not valid
-
-// TODO: add error msg if form is not valid
-// TODO: add form validation for each input
-// TODO: save form data if validation fails
