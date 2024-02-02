4e449cea427e2cce37161bd0066b36ec6f8860f9
KpryKorn <roffinisacha@gmail.com>
2024-02-02 11:32:03 +0100

    Ajouter des messages d'erreur pour les champs du formulaire

--- modal.js
+++ modal.js
@@ -6,7 +6,6 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 
 const formContainer = document.getElementById("formContainer");
 const formConfirmationModal = document.getElementById("formConfirmationModal");
-const formErrorMsg = document.getElementById("formNotValid");
 
 const closeModalBtn = document.getElementById("closeModalBtn");
 const closeConfirmationModalBtn = document.getElementById(
@@ -14,27 +13,125 @@ const closeConfirmationModalBtn = document.getElementById(
 );
 const submitBtn = document.getElementById("submitBtn");
 
+const firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+const lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
+const emailErrorMsg = document.getElementById("emailErrorMsg");
+const birthdateErrorMsg = document.getElementById("birthdateErrorMsg");
+const quantityErrorMsg = document.getElementById("quantityErrorMsg");
+const locationErrorMsg = document.getElementById("locationErrorMsg");
+const termsErrorMsg = document.getElementById("termsErrorMsg");
+
 /** ------------------------ FORM DATA VALUES ------------------------ **/
 const formInputs = {
   firstName: document.getElementById("firstName"),
   lastName: document.getElementById("lastName"),
   email: document.getElementById("email"),
+  birthdate: document.getElementById("birthdate"),
   quantity: document.getElementById("quantity"),
   location: document.querySelector('input[name="location"]'),
+  terms: document.getElementById("checkbox1"),
 };
+
+/** ------------------------ FORM REGEXP ------------------------ **/
+const nameRegExp = new RegExp(`^(?=.{2,25}$)[a-zA-Z]+(?:[-\s][a-z]+)*$`);
+const emailRegExp = new RegExp(
+  `^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$`
+);
+
+/** ------------------------ FORM ERROR MSG ------------------------ **/
+const errorMsg = {
+  firstName: "Le champ du prénom n'est pas valide ou incomplet",
+  lastName: "Le champ du nom n'est pas valide ou incomplet",
+  email: "Le champ de l'email n'est pas valide ou incomplet",
+  birthdate: "La date de naissance n'est pas valide ou incomplète",
+  quantity: "Veuillez indiquer le nombre de tournois",
+  location: "Veuillez sélectionner une ville",
+  terms: "Vous devez accepter les conditions d'utilisation",
+};
+
+// validate first name
+function validateFirstNameInput() {
+  if (!nameRegExp.test(formInputs.firstName.value))
+    return (firstNameErrorMsg.textContent = errorMsg.firstName);
+  else {
+    return (firstNameErrorMsg.textContent = "");
+  }
+}
+formInputs.firstName.addEventListener("change", validateFirstNameInput);
+
+// validate last name
+function validateLastNameInput() {
+  if (!nameRegExp.test(formInputs.lastName.value))
+    return (lastNameErrorMsg.textContent = errorMsg.lastName);
+  else {
+    return (lastNameErrorMsg.textContent = "");
+  }
+}
+formInputs.lastName.addEventListener("change", validateLastNameInput);
+
+// validate email
+function validateEmailInput() {
+  if (!emailRegExp.test(formInputs.email.value))
+    return (emailErrorMsg.textContent = errorMsg.email);
+  else {
+    return (emailErrorMsg.textContent = "");
+  }
+}
+formInputs.email.addEventListener("change", validateEmailInput);
+
+// validate birthdate
+function validateBirthdateInput() {
+  if (formInputs.birthdate.value === "")
+    return (birthdateErrorMsg.textContent = errorMsg.birthdate);
+  else {
+    return (birthdateErrorMsg.textContent = "");
+  }
+}
+formInputs.birthdate.addEventListener("change", validateBirthdateInput);
+
+// validate quantity
+function validateQuantityInput() {
+  if (formInputs.quantity.value === "")
+    return (quantityErrorMsg.textContent = errorMsg.quantity);
+  else {
+    return (quantityErrorMsg.textContent = "");
+  }
+}
+formInputs.quantity.addEventListener("input", validateQuantityInput);
+
+// validate location
+function validateLocationInput() {
+  if (!formInputs.location.checked)
+    return (locationErrorMsg.textContent = errorMsg.location);
+  else {
+    return (locationErrorMsg.textContent = "");
+  }
+}
+formInputs.location.addEventListener("change", validateLocationInput);
+
+// validate terms
+function validateTermsInput() {
+  if (!formInputs.terms.checked)
+    return (termsErrorMsg.textContent = errorMsg.terms);
+  else {
+    return (termsErrorMsg.textContent = "");
+  }
+}
+formInputs.terms.addEventListener("change", validateTermsInput);
 
 /** ------------------------ FORM VALIDATION ------------------------ **/
 let formIsValid = false;
+submitBtn.disabled = true;
 
 // TODO: add checkbox validation & location validation
 function validateForm() {
   // check tous les inputs de l'objet et return true si tous les inputs sont pas vides
-  formIsValid = Object.values(formInputs).every((element) => {
-    if (element.value !== "") return true;
-  });
+  formIsValid =
+    Object.values(formInputs).every((input) => input.value !== "") &&
+    formInputs.location.checked &&
+    formInputs.terms.checked;
 
   if (formIsValid) {
-    formErrorMsg.textContent = "";
     submitBtn.disabled = false;
     formContainer.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -43,19 +140,13 @@ function validateForm() {
     });
   } else {
     submitBtn.disabled = true;
-    formErrorMsg.textContent = "Le formulaire d'inscription n'est pas valide";
   }
-
-  console.log(formIsValid);
 }
 
 // check si le formulaire est valide à chaque onChange
 Object.values(formInputs).forEach((input) => {
-  input.addEventListener("change", validateForm);
+  input.addEventListener("input", validateForm);
 });
-
-// TODO: add form validation for each input
-// TODO: save form data if validation fails
 
 /** ------------------------ HANDLING MODALS ------------------------ **/
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
